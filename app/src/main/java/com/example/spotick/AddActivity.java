8d6ac01f9c0289e9338ca5d0fb599bc6a6b55749
totoczko <m.toloczko1@gmail.com>
2019-04-08 20:37:06 +0200

    added geo and uploading files

--- app/src/main/java/com/example/spotick/AddActivity.java
+++ app/src/main/java/com/example/spotick/AddActivity.java
@@ -1,21 +1,42 @@
 package com.example.spotick;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.List;
+import java.util.Locale;
 
 
 public class AddActivity extends AppCompatActivity {
 
     private FirebaseDatabase database;
     private DatabaseReference databaseRef;
-
+    private LocationManager locationManager;
+    private double longitude;
+    private double latitude;
+    private static final int MY_PERMISSION_REQUEST_LOCATION = 1;
+    public static final int PICK_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +74,75 @@ public class AddActivity extends AppCompatActivity {
             }
         });
 
+
+        if(ContextCompat.checkSelfPermission(AddActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(AddActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION)){
+                ActivityCompat.requestPermissions(AddActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSION_REQUEST_LOCATION);
+            }else{
+                ActivityCompat.requestPermissions(AddActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSION_REQUEST_LOCATION);
+            }
+        }else{
+            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            try {
+                String city = hereLocation(location.getLatitude(), location.getLongitude());
+                Log.d("CITY", "onCreate: " + city);
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(this, "Not found!", Toast.LENGTH_SHORT).show();
+            }
+        }
+
     }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,  int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSION_REQUEST_LOCATION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if(ContextCompat.checkSelfPermission(AddActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        try{
+                            String city = hereLocation(location.getLatitude(), location.getLongitude());
+                            Log.d("CITY", "onCreate: " + city);
+                        }catch(Exception e){
+                            e.printStackTrace();
+                            Toast.makeText(this, "Not found!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }else{
+                    Toast.makeText(this, "Permission not granted", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
+        }
+    }
+
+    public String hereLocation(double lat, double lon){
+        String cityName = "";
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        List<Address> addresses;
+        try{
+            addresses = geocoder.getFromLocation(lat, lon, 1);
+            if(addresses.size() > 0){
+                cityName = addresses.get(0).getLocality();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return cityName;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PICK_IMAGE) {
+            Uri fullPhotoUri = data.getData();
+            ImageView uploadedImage = findViewById(R.id.uploaded_image);
+            uploadedImage.setImageURI(fullPhotoUri);
+        }
+    }
+
 
 }
--- app/src/main/java/com/example/spotick/AddFile.java
+++ app/src/main/java/com/example/spotick/AddFile.java
@@ -1,17 +1,33 @@
 package com.example.spotick;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class AddFile extends Fragment {
+
+    public static final int PICK_IMAGE = 1;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.add_file, container, false);
+
+        final Button upload = view.findViewById(R.id.add_file_button);
+        upload.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                getActivity().startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+            }
+        });
+
         return view;
 
     }
+
 }
