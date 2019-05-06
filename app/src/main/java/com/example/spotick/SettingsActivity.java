951b7ba09b6bcba4279e9e46bb40138a3093703d
totoczko <m.toloczko1@gmail.com>
2019-05-06 20:18:11 +0200

    switch with notifications

--- app/src/main/java/com/example/spotick/SettingsActivity.java
+++ app/src/main/java/com/example/spotick/SettingsActivity.java
@@ -6,7 +6,9 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,14 +17,19 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.iid.FirebaseInstanceId;
 
 public class SettingsActivity extends AppCompatActivity {
 
     private static final String TAG = "SettingsActivity";
     private FirebaseDatabase database;
-    private DatabaseReference usersRef;
+    private DatabaseReference userRef;
+    private DatabaseReference tokenRef;
     private FirebaseAuth mAuth;
 
     @Override
@@ -102,7 +109,8 @@ public class SettingsActivity extends AppCompatActivity {
 
             String uid = currentUser.getUid();
             database = FirebaseDatabase.getInstance();
-            usersRef = database.getReference().child("users").child(uid);
+            userRef = database.getReference().child("users").child(uid);
+            tokenRef = database.getReference().child("tokens");
 
             final String name = currentUser.getDisplayName();
             String email = currentUser.getEmail();
@@ -133,7 +141,7 @@ public class SettingsActivity extends AppCompatActivity {
                                 @Override
                                 public void onComplete(@NonNull Task<Void> task) {
                                     if (task.isSuccessful()) {
-                                        usersRef.child("username").setValue(newLogin);
+                                        userRef.child("username").setValue(newLogin);
                                         currentLogin.setText(newLogin);
                                         Toast.makeText(SettingsActivity.this, "Profil zaktualizowany!", Toast.LENGTH_SHORT).show();
                                     }
@@ -151,7 +159,7 @@ public class SettingsActivity extends AppCompatActivity {
                                 @Override
                                 public void onComplete(@NonNull Task<Void> task) {
                                     if (task.isSuccessful()) {
-                                        usersRef.child("email").setValue(newEmail);
+                                        userRef.child("email").setValue(newEmail);
                                         currentEmail.setText(newEmail);
                                         Toast.makeText(SettingsActivity.this, "Profil zaktualizowany!", Toast.LENGTH_SHORT).show();
                                     }else{
@@ -185,6 +193,36 @@ public class SettingsActivity extends AppCompatActivity {
                 }
             });
 
+
+            final Switch notifSwitch = (Switch) findViewById(R.id.notifications_switch);
+
+            userRef.child("token").addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot snapshot) {
+                    String token = (String) snapshot.getValue();
+                    if(token != null){
+                        notifSwitch.setChecked(true);
+                    }
+                }
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                }
+            });
+
+
+            notifSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if (isChecked) {
+                        String token= FirebaseInstanceId.getInstance().getToken();
+                        userRef.child("token").setValue(token);
+                        tokenRef.child(currentUser.getUid()).setValue(token);
+                    } else {
+                        Toast.makeText(SettingsActivity.this, "Off", Toast.LENGTH_SHORT).show();
+                        userRef.child("token").removeValue();
+                        tokenRef.child(currentUser.getUid()).removeValue();
+                    }
+                }
+            });
 
             Button signOut = findViewById(R.id.sign_out);
 
